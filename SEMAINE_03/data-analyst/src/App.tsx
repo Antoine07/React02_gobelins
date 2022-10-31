cbb3ea81103405a2d8993015f9b53cc01929e14b
Antoine07 <antoine.lucsko@gmail.com>
2022-10-31 11:13:06 +0100

    support de coursé

---
+++ SEMAINE_03/data-analyst/src/App.tsx
@@ -0,0 +1,16 @@
+
+import { Container, Column, Row } from './Styles/Grid';
+import GlobalStyle from './Styles/Global';
+
+function App() {
+  return (
+    <Container>
+      <GlobalStyle />
+      <Column>
+        <Row></Row>
+      </Column>
+    </Container>
+  );
+}
+
+export default App;
